02af818e2c838ba1a918fc095cf92522fd502bd8
Aaro Häkkinen <aaroh@kkinen.com>
2019-10-19 01:33:33 +0300

    Part 3

---
+++ part3/backend/build/precache-manifest.497ade765d927ffcac9a5eca4b4f3765.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4ff382c09d6e159d2e2c500ff0bf6a1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f49d0ebac45906935d",
+    "url": "/static/css/main.33174cf0.chunk.css"
+  },
+  {
+    "revision": "b6e6c6fba4df28d5462c",
+    "url": "/static/js/2.290f2c33.chunk.js"
+  },
+  {
+    "revision": "27f49d0ebac45906935d",
+    "url": "/static/js/main.43db27ac.chunk.js"
+  },
+  {
+    "revision": "b05261b57c7547ee177e",
+    "url": "/static/js/runtime-main.d2c56390.js"
+  }
+]);
